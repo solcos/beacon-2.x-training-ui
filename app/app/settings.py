c35df7526ecf6596adee1573353705d6e0b23be6
solcos <92781950+solcos@users.noreply.github.com>
2023-03-27 14:11:37 +0200

    Update settings.py
    Add the variable from conf.py file to ALLOWED_HOSTS

--- app/app/settings.py
+++ app/app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from . import conf
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-3pdl)^a8o6#bft2!5$abk4+8eq=(xbc$xow$-c3+8xw=cr%u%o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['{0}'.format(conf.database_ip),'localhost']
 
 
 # Application definition
